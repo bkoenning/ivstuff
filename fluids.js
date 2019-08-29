6c3f1dc57881891918e7e5fcedc8460b947d24ed
Brandon Koenning <brandon.koenning@gmail.com>
2019-08-29 21:18:56 +0000

    dict

--- fluids.js
+++ fluids.js
@@ -1,12 +1,18 @@
-const fluids = [
-	{ 
-		name: "Lactated Ringers",
+'use strict'
+const fluids = { lactatedRingers: {
+        name: "Lactated Ringers",
 		possibleNames: ["LR", "Lactated Ringers"],
-		containsCompounds: [ { compound: "Sodium Chloride", amount: 0.6, unit: "g", per: { vol: 100, type: "mL"} },
-					{ compound: "Sodium Lactate", amount: 0.31, unit: "g", per: { vol: 100, type: "mL"} },
-					{ compound: "Potassium Chloride", amount: 0.03, unit: "g", per: {vol: 100, type: "mL"} },
-					{ compound: "Calcium Chloride Dihydrate", amount: 0.02, unit: "g", per: {vol: 100, type:"mL"} }
-				],
+		//containsCompounds: [ { compound: "Sodium Chloride", amount: 0.6, unit: "g", per: { vol: 100, type: "mL"} },
+		//			{ compound: "Sodium Lactate", amount: 0.31, unit: "g", per: { vol: 100, type: "mL"} },
+		//			{ compound: "Potassium Chloride", amount: 0.03, unit: "g", per: {vol: 100, type: "mL"} },
+		//			{ compound: "Calcium Chloride Dihydrate", amount: 0.02, unit: "g", per: {vol: 100, type:"mL"} }
+		//		],
+		containsCompounds: {
+		    sodiumChloride : {name: "Sodium Chloride", amount: 0.6, unit: "g", perVolume: {amount: 100, unit: "mL"}},
+		    sodiumLactate: {name: "Sodium Lactate", amount: 0.31, unit: "g", perVolume: {amount: 100, unit: "mL"}},
+		    potassiumChloride: {name: "Potassium Chloride", amount: 0.03, unit: "g", perVolume: {amount: 100, unit: "mL"}},
+		    calciumChlorideDihydrate: {name: "Calcium Chloride Dihydrate", amount: 0.02, unit:"g", perVolume: {amount: 100, unit: "mL"}}
+		},
 		containsElectrolytes: [ { electrolyte: "Sodium", amount: 130, unit: "mEq", per: {vol: 1, type: "L"} },
 						{ electrolyte: "Potassium", amount: 4, unit: "mEq", per: {vol: 1, type: "L"} },
 						{ electrolyte: "Calcium", amount: 3, unit: "mEq", per: {vol: 1, type: "L"} },
@@ -16,7 +22,7 @@ const fluids = [
 		osmolarity: {value: 275, unit: "mOsm", per: {vol: 1, type: "L"}},
 		hasCaloricValue: false
 	},
-	{
+	normalsodiumchloride: {
 		name: "0.9% Sodium Chloride",
 		possibleNames: ["Normal Saline", "0.9% NS", "NS", "0.9%NS", "0.9% Sodium Chloride", "0.9%Sodium Chloride", "FNS"],
 		containsCompounds: [ { compound: "Sodium Chloride", amount: 0.9, unit: "g", per: {vol: 100, type: "mL"} } ],
@@ -26,7 +32,7 @@ const fluids = [
 		osmolarity: { value: 308, unit: "mOsm", per: {vol: 1, type: "L"} },
 		hasCaloricValue: false
 	},
-	{
+	halfnormalsodiumchloride: {
 		name: "0.45% Sodium Chloride",
 		possibleNames: ["Half Normal Saline", "0.45% NS", "1/2NS", "0.45%NS", "0.45% Sodium Chloride", "0.45%Sodium Chloride", "Half-Normal Saline", "HNS"],
 		containsCompounds: [ { compound: "Sodium Chloride", amount: 0.45, unit: "g", per: {vol: 100, type: "mL"} } ],
@@ -36,7 +42,7 @@ const fluids = [
 		osmolarity: { value: 154, unit: "mOsm", per: {vol: 1, type: "L"} },
 		hasCaloricValue: false
 	},
-	{
+	quarternormalsodiumchloride: {
 		name: "0.225% Sodium Chloride",
 		possibleNames: ["Quarter Normal Saline", "0.225% NS", "1/4NS", "0.225%NS", "0.225% Sodium Chloride", "0.225%Sodium Chloride", "Quarter-Normal Saline", "QNS"],
 		containsCompounds: [ { compound: "Sodium Chloride", amount: 0.225, unit: "g", per: {vol: 100, type: "mL"} } ],
@@ -46,12 +52,13 @@ const fluids = [
 		osmolarity: { value: 77, unit: "mOsm", per: {vol: 1, type: "L"} },
 		hasCaloricValue: false
 	},
-	{
+	d5: {
 		name: "Dextrose 5% in Water",
 		possibleNames: ["D5W", "D5%W", "Dextrose 5% in Water", "D5", "D5%", "Dextrose 5%", "Dextrose5%"],
 		hasCaloricValue: true
 	}
-];
+};
+module.exports = fluids;
 /*
 let s = "sodium";
 let regS = RegExp(s.trim(), "ig");
